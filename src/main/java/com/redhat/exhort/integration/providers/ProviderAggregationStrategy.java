bf0441f12676650294326053623102cc6bc57cb3
Chao Wang <chaowan@redhat.com>
2025-10-22 10:07:14 +0800

    chore: update license header

--- src/main/java/com/redhat/exhort/integration/providers/ProviderAggregationStrategy.java
+++ src/main/java/com/redhat/exhort/integration/providers/ProviderAggregationStrategy.java
@@ -1,6 +1,5 @@
 /*
- * Copyright 2023 Red Hat, Inc. and/or its affiliates
- * and other contributors as indicated by the @author tags.
+ * Copyright 2023-2025 Trustify Dependency Analytics Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
